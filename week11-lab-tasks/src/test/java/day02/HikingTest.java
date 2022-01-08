3cefb62812e45c26bd7e1198c567e9fd8f238fe7
JaniVarga86HUN <jani.varga86@gmail.com>
2022-01-08 23:16:20 +0100

    To the task of daily consultation day02, week11, extra test methods are added.

--- week11-lab-tasks/src/test/java/day02/HikingTest.java
+++ week11-lab-tasks/src/test/java/day02/HikingTest.java
@@ -3,6 +3,7 @@ package day02;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +12,17 @@ class HikingTest {
     @Test
     void testHiking() {
         Hiking hiking = new Hiking();
-        assertEquals(13, hiking.getPlusElevation(
+        assertEquals(13., hiking.getPlusElevation(
                 Arrays.asList(10., 20., 15., 18.)));
     }
 
+    @Test
+    void testFullyElevation() {
+        assertEquals(20., new Hiking().getPlusElevation(List.of(10., 14., 20., 23., 28., 30.)));
+    }
+
+    @Test
+    void testFullySlope() {
+        assertEquals(0, new Hiking().getPlusElevation(List.of(30., 26., 22., 19., 13., 7., 2.)));
+    }
 }
